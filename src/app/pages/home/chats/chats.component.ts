f23af86b677b6151b9a1382838548fb76699b68b
estephanysvg <estephanysvg@gmail.com>
2024-06-13 19:48:36 +0200

    Cambios de fondos, quitar habilidades, quitar contenido del footer, quitar footer estudiantes y cambio de los cards

--- src/app/pages/home/chats/chats.component.ts
+++ src/app/pages/home/chats/chats.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-chats',
@@ -9,5 +10,33 @@ import { RouterModule } from '@angular/router';
   styleUrl: './chats.component.css'
 })
 export default class ChatsComponent {
+
+  @ViewChild('dropdownButton') dropdownButton!: ElementRef;
+  @ViewChild('dropdownMenu') dropdownMenu!: ElementRef;
+  @ViewChild('searchInput') searchInput!: ElementRef;
+
+  isOpen: boolean = false;
+
+  toggleDropdown(): void {
+    this.isOpen = !this.isOpen;
+    this.dropdownMenu.nativeElement.classList.toggle('hidden', !this.isOpen);
+    if (this.isOpen) {
+      setTimeout(() => {
+        this.searchInput.nativeElement.focus();
+      }, 0);
+    }
+  }
+
+  filterItems(): void {
+    const searchTerm = this.searchInput.nativeElement.value.toLowerCase();
+    const items = this.dropdownMenu.nativeElement.querySelectorAll('a');
+
+    items.forEach((item: any) => {
+      const text = item.textContent.toLowerCase();
+      item.style.display = text.includes(searchTerm) ? 'block' : 'none';
+    });
+  }
+
+
 
 }
